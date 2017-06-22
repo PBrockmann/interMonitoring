5990a617d76c405ae9b5a0fc091f095cfe56bdf6
Compte_de_Service WEBSERVICES <webservices@webservices2017.ipsl.upmc.fr>
2017-06-22 21:43:51 +0200

    Fix date offset argument when argument is specified (*>TRUE)

--- test.php
+++ test.php
@@ -6,7 +6,7 @@
 
 <UL>
 
-<FORM name="formchoice" method="post" action="http://webservices2017.ipsl.fr/interMonitoring_fromHermes/index.php" target="_blank">
+<FORM name="formchoice" method="post" action="http://webservices2017.ipsl.fr/interMonitoring_prefilled/index.php" target="_blank">
 
 <input type="text" name="simul1" size=100 value="http://esgf.extra.cea.fr/thredds/catalog/work/p86caub/IPSLCM5A/PROD/1pctCO2/1pctCO22">
 <BR>
